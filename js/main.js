c5ce25e1ffe326bc89e8a739eebcd45bdcd74a83
yasnovskiy <andrey5.1997@mail.ru>
2020-09-27 15:58:11 +0500

    Получил шаблон

--- js/main.js
+++ js/main.js
@@ -4,32 +4,53 @@ let pins = document.querySelectorAll('.map__pin');
 // Получаем элемент с идентификатором "housing-price". Используем селектор.
 let housePriceField = document.querySelector('#housing-price');
 
+const getRandomInt = function (max) {
+  return  Math.floor(Math.random() * max);
+};
 
+let titleArray = ['Title1', 'Title2', 'Title3'];
+let photosArray = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+let checkArray = ['12:00', '13:00', '14:00'];
+let featuresArray = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+let typeArray = ['palace', 'flat', 'house', 'bungalow'];
 
-let generateObject = function () {
+let generateObject = function (num) {
   const ad = {
-    author: {},
-    offer: {},
-    location: {}
+    author: {
+      avatar: 'img/avatars/user0' + (num + 1) + '.png'
+    },
+    offer: {
+      title: titleArray[Math.floor(Math.random() * titleArray.length)],
+      address: "{{location.x}}, {{location.y}}",
+      price: Math.floor(Math.random() * (5001 - 0)) + 0,
+      rooms: Math.floor(Math.random() * (3 - 1)) + 1,
+      type: typeArray[Math.floor(Math.random() * typeArray.length)],
+      photos: photosArray.slice(getRandomInt(photosArray.length)),
+      description: '',
+      checkin: checkArray[Math.floor(Math.random() * checkArray.length)],
+      checkout: checkArray[Math.floor(Math.random() * checkArray.length)],
+      features: featuresArray.slice(getRandomInt(featuresArray.length - 1))
+    },
+    location: {
+      x: Math.floor(Math.random() * (1200 - 0)),
+      y: Math.floor(Math.random() * (630 - 130))
+    }
   };
-  let avatarRnd = Math.floor(Math.random() * Math.floor(9));
-  // let avatarRnd = Math.floor(Math.random() * (8 - 1) + 1);
-  let titleArray = ['Title1', 'Title2', 'Title3'];
-  let photosArray = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
-  let check = ['12:00', '13:00', '14:00'];
-
-  ad.author.avatar = 'img/avatars/user0' + avatarRnd + '.png';
-  ad.offer.title = titleArray[Math.floor(Math.random() * titleArray.length)];
-  // ad.offer.address = titleArray[Math.floor(Math.random() * Math.floor(titleArray.lenght - 1))] ;
-  ad.offer.photos = photosArray[Math.floor(Math.random() * photosArray.length)];
-  ad.offer.checkin = check[Math.floor(Math.random() * check.length)];
-  ad.offer.checkout = check[Math.floor(Math.random() * check.length)];
   return ad;
 };
 
-
 let objectArray = [];
 for (let i = 0; i < 8; i++) {
-  objectArray.push(generateObject());
-  console.log(objectArray)
+  objectArray.push(generateObject(i));
 };
+
+let similarListmMapPins = document.querySelector('.map__pins');
+let similarWizardPin = document.querySelector('#pin').content.querySelector('.map__pin ');
+
+for (let i = 0; i < 1; i++) {
+  let mark = similarWizardPin.cloneNode(true);
+
+  similarListmMapPins.appendChild(mark);
+};
+
+console.log(objectArray);
